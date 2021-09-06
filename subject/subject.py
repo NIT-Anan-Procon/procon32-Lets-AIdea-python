c000b54e9435c15dc0d782c9b362c001ca89bdcd
Mitsuki4123 <mitsukikosen7@gmail.com>
2021-09-06 19:51:30 +0900

    文章から主語を抜き出す機能の追加

---
+++ subject/subject.py
@@ -0,0 +1,24 @@
+import spacy
+
+
+def parse_document(sentence, nlp):
+    doc = nlp(sentence)
+    tokens = []
+
+    ## 参照しやすいようにトークンのリストを作る
+    for sent in doc.sents:
+        for token in sent:
+            tokens.append(token)
+
+    subject_list = []
+
+    for token in tokens:
+        ## 依存関係ラベルがnsubj or iobjであれば「＜見出し語＞:＜係り先の見出し語＞」をリストに追加する。
+        if token.dep_ in ["nsubj", "iobj"]:
+            subject_list.append(f"{token.lemma_}")
+
+    return subject_list
+
+
+nlp = spacy.load("ja_ginza")
+print(parse_document("犬が草原を駆け回っている。", nlp))
