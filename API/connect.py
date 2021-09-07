0d4c79b7cca8912a9c6b144a85dff44872cf775d
Mitsuki4123 <mitsukikosen7@gmail.com>
2021-09-07 11:29:55 +0000

    開発サーバーの内容を一時的にpush

--- API/connect.py
+++ API/connect.py
@@ -5,11 +5,15 @@ app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # jsonの文字化け防止
 
 
-@app.route("/NGword", methods=["GET"])
+@app.route("/~kurabuchi/procon32_Lets_AIdea_python/API/connect.py")
 def NGword():
     url = request.json["url"]
-    return jsonify("あいうえお")
+    sample = {
+        'url':url,
+        'word':'あいうえお'
+    }
+    return jsonify(sample)
 
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run(port=5000, debug="True")
