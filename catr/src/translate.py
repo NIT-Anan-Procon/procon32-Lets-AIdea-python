0a13c05f7f0633bb1666d2e5c709cfe0f294e9bc
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-09-09 01:40:45 +0000

    フォーマッター類の追加 #28

--- catr/src/translate.py
+++ catr/src/translate.py
@@ -1,44 +1,40 @@
-﻿import argparse
 import json
 import os
-import sys
 import urllib.parse
 import urllib.request
 
-with open(os.path.dirname(os.path.abspath(__file__)) + '/../config.json', encoding='utf_8_sig') as j:
-	config = json.load(j)
+with open(
+    os.path.dirname(os.path.abspath(__file__)) + "/../config.json", encoding="utf_8_sig"
+) as j:
+    config = json.load(j)
 
-AUTH_KEY = config['auth_key']
-DEEPL_TRANSLATE_EP = 'https://api.deepl.com/v2/translate'
+AUTH_KEY = config["auth_key"]
+DEEPL_TRANSLATE_EP = "https://api.deepl.com/v2/translate"
+
 
 def translate(text):
-	s_lang=''
-	t_lang='JA'
-	headers = {
-		'Content-Type': 'application/x-www-form-urlencoded; utf-8'
-	}
+    print("aiueo")
+    s_lang = ""
+    t_lang = "JA"
+    headers = {"Content-Type": "application/x-www-form-urlencoded; utf-8"}
 
-	params = {
-		'auth_key': AUTH_KEY,
-		'text': text,
-		'target_lang': t_lang
-	}
+    params = {"auth_key": AUTH_KEY, "text": text, "target_lang": t_lang}
 
-	if s_lang != '':
-		params['source_lang'] = s_lang
+    if s_lang != "":
+        params["source_lang"] = s_lang
 
-	req = urllib.request.Request(
-		DEEPL_TRANSLATE_EP,
-		method='POST',
-		data=urllib.parse.urlencode(params).encode('utf-8'),
-		headers=headers
-	)
+    req = urllib.request.Request(
+        DEEPL_TRANSLATE_EP,
+        method="POST",
+        data=urllib.parse.urlencode(params).encode("utf-8"),
+        headers=headers,
+    )
 
-	try:
-		with urllib.request.urlopen(req) as res:
-			res_json = json.loads(res.read().decode('utf-8'))
-			sentence_bef = str(res_json)
-			sentence_aft = sentence_bef[62:-4]
-			return sentence_aft
-	except urllib.error.HTTPError as e:
-		print(e)
+    try:
+        with urllib.request.urlopen(req) as res:
+            res_json = json.loads(res.read().decode("utf-8"))
+            sentence_bef = str(res_json)
+            sentence_aft = sentence_bef[62:-4]
+            return sentence_aft
+    except urllib.error.HTTPError as e:
+        print(e)
