35019dbadc85e6082621c81b23f9ee33cabc500d
Mitsuki4123 <mitsukikosen7@gmail.com>
2021-09-09 01:10:10 +0000

    URLをurl_image.pyに渡す

--- API/connect.py
+++ API/connect.py
@@ -1,6 +1,10 @@
 import json
 from flask import Flask, request, jsonify  # flaskを使って実装
 
+import sys
+sys.path.append('../')
+from url_image.url_image import image
+
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # jsonの文字化け防止
 
@@ -8,6 +12,7 @@ app.config["JSON_AS_ASCII"] = False  # jsonの文字化け防止
 @app.route("/~kurabuchi/procon32_Lets_AIdea_python/API/connect.py")
 def NGword():
     url = request.json["url"]
+    image(url)
     sample = {
         'url':url,
         'word':'あいうえお'
