70f3c6e127ee08cdc196993800cf1e1150b79fe3
Mitsuki4123 <mitsukikosen7@gmail.com>
2021-09-12 00:47:56 +0000

    URLとコードの整理

--- API/connect.py
+++ API/connect.py
@@ -4,15 +4,15 @@ import sys
 sys.path.append('../')
 from url_image.url_image import image
 
-import sys
-sys.path.append('../')
+# import sys
+# sys.path.append('../')
 from catr import predict
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # jsonの文字化け防止
 
 
-@app.route("/test")
+@app.route("/")
 def NGword():
     url = request.json["url"]
     image(url)
